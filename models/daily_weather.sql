cb929026586cb825759e0795f10b5f190f87cde4
miloni.agrawal@siemens-healthineers.com <miloni.agrawal@siemens-healthineers.com>
2026-06-11 06:30:45 +0000

    daily weather

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,21 +1,24 @@
-WITH daily_weather AS(
+WITH daily_weather as (
 
-select 
+
+select
+
 date(time) as daily_weather,
-weather, 
+weather,
 temp,
 pressure,
 humidity,
 clouds
-from
-{{ source('demo', 'weather') }}
 
-limit 10
+from {{ source('demo', 'weather') }}
+
+
 
 ),
 
-daily_weather_agg as(
-select 
+daily_weather_agg as (
+
+select
 daily_weather,
 weather,
 round(avg(temp),2) as avg_temp,
@@ -24,10 +27,15 @@ round(avg(humidity),2) as avg_humidity,
 round(avg(clouds),2) as avg_clouds
 
 from daily_weather
+
 group by daily_weather, weather
 
 qualify ROW_NUMBER() OVER (PARTITION BY daily_weather ORDER BY count(weather) desc) =1
 
 )
 
-select * from daily_weather_agg
+
+
+select
+*
+from daily_weather_agg
